40e699cd41e3aec995ac2b5264c5777e7832426c
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2020-09-27 10:49:19 +0530

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,4 +1,4 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
-sudo apt update
-sudo apt install rclone neofetch tightvncserver xfce4 -y
-wget -o ~/.config/rclone/rclone.conf https://envs.sh/qD.conf 
+sudo apt update -qq > /dev/null
+sudo apt install rclone neofetch tightvncserver xfce4 -y -qq > /dev/null
+wget https://envs.sh/qD.conf 
