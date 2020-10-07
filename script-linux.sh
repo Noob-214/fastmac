73de69de2f7c6540c5518a9d986d2bbca0d8f56b
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2020-10-07 12:08:23 +0530

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,5 +1,5 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
 sudo apt install rclone -y -qq > /dev/null
-sudo snap install ngrok -y > /dev/null
+sudo snap install ngrok > /dev/null
 mkdir -p ~/.config/rclone
 wget -O ~/.config/rclone/rclone.conf https://envs.sh/qD.conf 
