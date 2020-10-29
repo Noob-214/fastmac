60ca81340459d92728813695b608f09a4685e6f6
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2020-10-29 23:12:23 +0530

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,5 +1,6 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
-sudo apt install rclone fish neofetch -y -qq > /dev/null
+sudo apt install rclone fish ffmpeg -y -qq > /dev/null
 sudo snap install ngrok > /dev/null
 mkdir -p ~/.config/rclone
 wget -O ~/.config/rclone/rclone.conf https://envs.sh/qD.conf 
+
