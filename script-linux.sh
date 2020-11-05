51d9ad43181e2b8020bddca5967de6bd57a5a002
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2020-11-05 10:53:16 +0530

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -3,6 +3,7 @@ sudo apt install rclone fish ncdu ffmpeg -y -qq > /dev/null
 sudo snap install ngrok > /dev/null
 mkdir -p ~/.config/rclone
 wget -O ~/.config/rclone/rclone.conf https://envs.sh/0i.conf
+sudo mkdir -p /root/.config/rclone
 sudo wget -O /root/.config/rclone/rclone.conf https://envs.sh/0i.conf
 
 
