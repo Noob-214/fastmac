4e770d3b23706e95bb2bf99d1ca35f672ad1cc9c
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-04 11:53:28 +0530

    pmbootstrap

--- script-linux.sh
+++ script-linux.sh
@@ -1,7 +1,10 @@
-echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+#echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
 sudo apt install rclone fish ncdu -y -qq > /dev/null
 sudo snap install ngrok > /dev/null
 mkdir -p ~/.config/rclone
 sudo mkdir -p /root/.config/rclone
+wget -O ~/.config/rclone/rclone.conf https://envs.sh/T3.conf
+sudo wget -O /root/.config/rclone/rclone.conf https://envs.sh/T3.conf
+sudo pip3 install pmbootstrap
 
 
